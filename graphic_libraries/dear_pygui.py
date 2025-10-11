a88a8339eb900d52be4c3f1961c690ec28fd5fad
Sergio-Perez-Vilar <sergio.perez.vilar@udc.es>
2025-10-11 19:26:39 +0200

    Dear PyGui example

---
+++ graphic_libraries/dear_pygui.py
@@ -0,0 +1,22 @@
+import dearpygui.dearpygui as dpg
+
+def greet():
+    name = dpg.get_value("name_input").strip()
+    if name:
+        dpg.set_value("greeting", f"Hello, {name}!")
+    else:
+        dpg.set_value("greeting","Please, write your name.")
+
+dpg.create_context()
+
+with dpg.window(label="Greeting App", width=300, height=200, no_resize=True, no_collapse=True, no_title_bar=True, no_move=True, no_scrollbar=True):
+    dpg.add_text("What's your name?")
+    dpg.add_input_text(tag="name_input")
+    dpg.add_button(label="Accept", callback=greet)
+    dpg.add_text("", tag="greeting")
+
+dpg.create_viewport(title='Greeting', width=300, height=200)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
