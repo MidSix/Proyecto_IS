4386bd1b4ba3aef464a7a5cd8b0a67c79d0e28f6
Acrafto <xoel.sanchez.dacoba@udc.es>
2025-10-28 17:54:28 +0100

    fea(Linear_regression.py): Linear regression thingy, humble begin

--- src/Linear_regression.py
+++ src/Linear_regression.py
@@ -1,25 +1,50 @@
-from data_module import *
+from data_module import DataModule
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression as Sklearn_LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.datasets import fetch_california_housing#this dataset is for testing purposes only
-#This for now is a testing archive for me to learn how to use scikit-learn for the implementation of linear models
-direct_data = fetch_california_housing()
-data=pd.DataFrame(direct_data.data, columns=direct_data.feature_names)
-dataframe=DataModule()
-dataframe.direct_data_load(data)
-X = dataframe.get_features()
-y = dataframe.get_target()
-model = LinearRegression()
-model.fit(X, y)
-y_pred = model.predict(X)
-mse = mean_squared_error(y, y_pred)
-r2 = r2_score(y, y_pred)
 
-print("Coeficiente:", model.coef_)
-print("Intercepto:", model.intercept_)
-print("Error cuadrático medio (MSE):", mse)
-print("Coeficiente de determinación (R²):", r2)
+#This model is to be highly integrated with the gui.
+#I don't think I need to add memorization or saving/loading capabilities here,
+#so this will simply be a model object that the gui can interact with to create and evaluate linear regression models.
+class LinearRegressionModel(Sklearn_LinearRegression):
+    def __init__(self, data=None, target=None, initialized=False, R2=None, MSE=None):
+        super().__init__()
+        self.initialized = False#to easily check if the model has been created
+        self.R2 = None
+        self.MSE = None
+    
+    @property
+    def is_initialized(self): #in case we need to check externally if the model is created
+        return self.initialized
+    
+    @property
+    def get_R2(self): #This two are to be called by gui after evaluation, to show results
+        if self.initialized:
+            return self.R2
+        return 9999#arbitrary high value to indicate uninitialized model, like if u get this you know something's wrong
+    
+    @property
+    def get_MSE(self):
+        if self.initialized:
+            return self.MSE
+        return 9999#arbitrary high value to indicate uninitialized model, like if u get this you know something's wrong
+
+    def create_linear_model(self, training_data:pd.DataFrame, training_target:pd.Series):#columns selected in the gui as features and target
+        self._data = training_data
+        self._target = training_target
+        self.fit(self._data, self._target)
+        return self
+    
+    def evaluate_model(self, test_data:DataModule, test_target):
+        self.initialized = True
+        predictions = self.predict(test_data)
+        self.MSE = mean_squared_error(test_target, predictions)
+        self.R2 = r2_score(test_target, predictions)
+        return self.MSE, self.R2   
+
 
 if __name__ == "__main__":
-    pass #This space in the future will include a simple test for the linear regression model, but for now it's not necessary
+    model = LinearRegressionModel()
+    example_data = fetch_california_housing(as_frame=True)
+    model.create_linear_model(example_data.data, example_data.target)
