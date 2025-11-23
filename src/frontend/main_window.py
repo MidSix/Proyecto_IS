39fab03cbecda8877079d2fc7ad9d53f38afa92c
Sebastian-David-Moreno-Exposito <sebamorexx@gmail.com>
2025-11-23 20:49:54 +0100

    feat(welcome_window): Adds a Quick Start guide
    Quick Start window stacked at index 0 in main_window. It's shown when the user opens the program and can be accessed whenever the user wants, I think it's better this way than just showing a message because the user isn't a robot, they can't memorize all the instruction step by step just by looking at them xd. So the user can always check the Quick Start window whenever they need it.

--- src/frontend/main_window.py
+++ src/frontend/main_window.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from frontend.setup_window import SetupWindow
 from frontend.result_window import ResultWindow
+from frontend.welcome_window import WelcomeWindow
 from PyQt5.QtCore import (
     pyqtSlot,
 )
@@ -12,22 +13,32 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Linear Regression - Main window")
-        #Stack windows------------------------------------
+        #Stack windows--------------------------------------------------
         self.stacked_widget = QStackedWidget()
+        self.welcome_window = WelcomeWindow(self.stacked_widget)
         self.setup_window = SetupWindow(self.stacked_widget)
         self.result_window = ResultWindow(self.stacked_widget)
 
-        self.stacked_widget.addWidget(self.setup_window)  # índice 0
-        self.stacked_widget.addWidget(self.result_window) # indice 1
-        #------------------Conections-----------------------
+        self.stacked_widget.addWidget(self.welcome_window) # índice 0
+        self.stacked_widget.addWidget(self.setup_window)  # índice 1
+        self.stacked_widget.addWidget(self.result_window) # indice 2
+        #------------------Conections-----------------------------------
         self.setup_window.another_file_opened.connect(self.another_file_opened)
         self.setup_window.train_test_df_ready.connect(self.train_test_df_ready)
         self.result_window.cant_be_plotted.connect(self.cant_be_plotted)
-        #---------------------------------------------------
+        #---------------------------------------------------------------
+        self.welcome_window_button = QPushButton("Quick Start")
+        self.welcome_window_button.clicked.connect(
+            self.change_to_welcome_window
+            )
         self.setup_window_button = QPushButton("Data Management")
-        self.setup_window_button.clicked.connect(self.change_to_setup_window)
+        self.setup_window_button.clicked.connect(
+            self.change_to_setup_window
+            )
         self.result_window_button = QPushButton("Model Managent")
-        self.result_window_button.clicked.connect(self.change_to_result_window)
+        self.result_window_button.clicked.connect(
+            self.change_to_result_window
+            )
         widgets = [self.setup_window_button,self.result_window_button]
 
         def hide_widgets():
@@ -38,6 +49,7 @@ class MainWindow(QWidget):
 
         #top layout - main bar:
         top_layout = QHBoxLayout()
+        top_layout.addWidget(self.welcome_window_button)
         top_layout.addWidget(self.setup_window_button)
         top_layout.addWidget(self.result_window_button)
 
@@ -53,10 +65,12 @@ class MainWindow(QWidget):
         main_layout.addWidget(self.stacked_widget)
         self.setLayout(main_layout)
     #Methods:
-    def change_to_setup_window(self):
+    def change_to_welcome_window(self):
         self.stacked_widget.setCurrentIndex(0)
-    def change_to_result_window(self):
+    def change_to_setup_window(self):
         self.stacked_widget.setCurrentIndex(1)
+    def change_to_result_window(self):
+        self.stacked_widget.setCurrentIndex(2)
 
     #MainWindow as the orchestrator, the one
     #which handle the communication between these two classes.
---
+++ src/frontend/welcome_window.py
@@ -0,0 +1,60 @@
+from PyQt5.QtWidgets import (
+    QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout,
+    QFileDialog, QTableView, QMessageBox, QHeaderView, QListWidget,
+    QAbstractItemView, QHBoxLayout, QComboBox, QSizePolicy
+)
+
+from PyQt5.QtCore import (
+    Qt,
+    pyqtSlot,
+    pyqtSignal
+)
+class WelcomeWindow(QWidget):
+    def __init__(self, stacked_widget):
+        super().__init__()
+        self.stacked_widget = stacked_widget
+        self.setWindowTitle("Quick Start")
+        self.layout = QVBoxLayout()
+#----------------------------set_label----------------------------------
+        self.welcome_message = QLabel()
+        self.welcome_message.setStyleSheet("""
+                                QLabel {
+                                font-family: 'Consolas';
+                                font-size: 12pt;
+                                color: #E0E0E0;
+                                }
+                                """)
+        self.layout.addWidget(self.welcome_message, alignment=Qt.AlignCenter)
+        #this is just some HTML to format the welcome message
+        self.welcome_message.setText(
+            "<h1><b>Welcome to the Linear Regression App</b></h1><br>"
+            "<b>1. Load data</b><br>"
+            "• Use the button 'Open File' in Data Management to open a file<br>"
+            "• Select between CSV, excel and MySQL data formats to load<br><br>"
+            "<b>2. Select features and target</b><br>"
+            "• Select input(features) columns<br>"
+            "• Select output(target) column<br><br>"
+            "<b>3. Confirm your selection</b><br>"
+            "• Just press the 'confirm selection button'<br><br>"
+            "<b>4. If you selected columns with NaN values</b><br>"
+            "• Press the dropdown and select one of the four<br>"
+            "• Then press 'Apply preprocessing to handle your missing data'<br><br>"
+            "<b>5. Split data into train and test sets</b><br>"
+            "• Fill both input fields <br>"
+            "• Use the 'create_model' button to split your data<br>"
+            "  and create your linear regression model<br>"
+            "• Now a summary message will appear besides where you pressed<br>"
+            "  'create_model' there you'll find a summary of your model<br><br>"
+            "<b>6. See your linear_regression</b><br>"
+            "• Just hit the 'Model Management' button on the top bar<br>"
+            "• There you'll find your parity graph and simple regression graph<br>"
+            "  (if simple otherwise you'll only find the parity graph)<br>"
+            "• consult usefull metrics such as R² and MSE for both train and test<br><br>"
+            "<b>7. Save your model</b><br>"
+            "• You're able to add a description before saving your model<br>"
+            "• To save your model just press the 'save model button'<br><br>"
+            "<b>8. Load your model</b><br>"
+            "• Just hit the 'load model' button and select a previosly saved model<br>"
+        )
+
+        self.setLayout(self.layout)
